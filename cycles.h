2718800a160021993fe3b25a1f3fe198567880bd
pallaria <joepallaria@gmail.com>
2018-07-28 01:36:06 -0700

    removing object files

--- cycles.h
+++
@@ -1,9 +0,0 @@
-/* SUMMARY
- * The game is driven from here. The initialize function creates the entities
- * and calls the place_ship function for each commander. The primary game logic
- * is in the play function. Each player takes turns attacking until a condition is met.
- */
-static void place_ships( void );
-
-extern void initialize( void );
-extern void play( void );
